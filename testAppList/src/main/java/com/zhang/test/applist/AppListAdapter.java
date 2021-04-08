d119408adc057a68c0d91449e053a0f69d63df24
ZhangXiaoMing <zhangxiaoming6672@163.com>
2021-04-08 20:08:52 +0800

    应用列表

--- testAppList/src/main/java/com/zhang/test/applist/AppListAdapter.java
+++ testAppList/src/main/java/com/zhang/test/applist/AppListAdapter.java
@@ -1,33 +1,32 @@
 package com.zhang.test.applist;
 
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-
 import com.zhang.library.adapter.BaseRecyclerAdapter;
 import com.zhang.library.adapter.viewholder.base.BaseRecyclerViewHolder;
+
+import androidx.annotation.NonNull;
 
 /**
  * @author ZhangXiaoMing 2021-04-04 11:22 星期日
  */
-public class AppListAdapter extends BaseRecyclerAdapter<PackageInfo> {
+public class AppListAdapter extends BaseRecyclerAdapter<ApplicationInfo> {
     @Override
-    protected BaseRecyclerViewHolder<PackageInfo> onCreateVHolder(ViewGroup parent, int viewType) {
+    protected BaseRecyclerViewHolder<ApplicationInfo> onCreateVHolder(ViewGroup parent, int viewType) {
         return new ViewHolder(parent);
     }
 
     @Override
-    protected void onBindData(BaseRecyclerViewHolder<PackageInfo> viewHolder, PackageInfo data, int position) {
+    protected void onBindData(BaseRecyclerViewHolder<ApplicationInfo> viewHolder, ApplicationInfo data, int position) {
 
     }
 
-    private static final class ViewHolder extends BaseRecyclerViewHolder<PackageInfo> {
+    private static final class ViewHolder extends BaseRecyclerViewHolder<ApplicationInfo> {
         private ImageView mIvIcon;
         private TextView mTvName;
 
@@ -43,15 +42,13 @@ public class AppListAdapter extends BaseRecyclerAdapter<PackageInfo> {
         }
 
         @Override
-        public void onBindData(PackageInfo item, int position) {
+        public void onBindData(ApplicationInfo item, int position) {
             PackageManager manager = itemView.getContext().getPackageManager();
 
-            ApplicationInfo info = item.applicationInfo;
-
-            Drawable drawable = info.loadIcon(manager);
+            Drawable drawable = item.loadIcon(manager);
             mIvIcon.setImageDrawable(drawable);
 
-            CharSequence label = manager.getApplicationLabel(info);
+            CharSequence label = manager.getApplicationLabel(item);
             mTvName.setText(label);
 //            mTvName.setText(info.loadLabel(manager));
         }
--- testAppList/src/main/java/com/zhang/test/applist/MainActivity.java
+++ testAppList/src/main/java/com/zhang/test/applist/MainActivity.java
@@ -1,19 +1,32 @@
 package com.zhang.test.applist;
 
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.zhang.library.adapter.callback.OnItemClickCallback;
+import com.zhang.library.utils.context.ContextUtils;
+import com.zhang.library.utils.context.FileUtils;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.Iterator;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Activity mActivity;
 
     private RecyclerView mRvContent;
 
@@ -22,15 +35,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContextUtils.set(this);
+        mActivity = this;
+
         setContentView(R.layout.activity_main);
 
         mRvContent = findViewById(R.id.rv_content);
         mRvContent.setLayoutManager(new GridLayoutManager(this, 5));
 
         adapter = new AppListAdapter();
+        adapter.getCallbackHolder().addOnItemClickCallback(new OnItemClickCallback<ApplicationInfo>() {
+            @Override
+            public void onItemClick(View itemView, ApplicationInfo data, int position) {
+                String sourceDir = data.sourceDir;
+                String publicSourceDir = data.publicSourceDir;
+                String content = "sourceDir = [" + sourceDir + "]\n"
+                        + "publicSourceDir = [" + publicSourceDir + "]";
+                Log.d(TAG, content);
+
+                File file = new File(sourceDir);
+                Log.d(TAG, "sourceDir file is exist ? " + file.exists());
+
+                String cachePath = FileUtils.getCachePath();
+                Log.d(TAG, "cachePath = " + cachePath);
+
+                FileUtils.copyFile(sourceDir, cachePath, "base.apk");
+            }
+        });
         mRvContent.setAdapter(adapter);
 
         processQueryAppList();
+        provisionManagedProfile();
     }
 
     @Override
@@ -40,14 +75,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void processQueryAppList() {
         PackageManager manager = getPackageManager();
 
-        List<PackageInfo> list = manager.getInstalledPackages(PackageManager.GET_META_DATA);
-        Iterator<PackageInfo> iterator = list.iterator();
+        List<ApplicationInfo> list = manager.getInstalledApplications(PackageManager.GET_META_DATA);
+        Iterator<ApplicationInfo> iterator = list.iterator();
         while (iterator.hasNext()) {
             //判断是否是系统应用
 
-            PackageInfo info = iterator.next();
-            boolean isSysApp = (info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1;
-            boolean isSysUpd = (info.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 1;
+            ApplicationInfo info = iterator.next();
+            boolean isSysApp = (info.flags & ApplicationInfo.FLAG_SYSTEM) == 1;
+            boolean isSysUpd = (info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 1;
 
             if (isSysApp || isSysUpd) {
                 iterator.remove();
@@ -55,11 +90,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         adapter.getDataHolder().setDataList(list);
-
-//        List<ApplicationInfo> list = manager.getInstalledApplications(PackageManager.GET_META_DATA);
-//        for (ApplicationInfo info : list) {
-//            builder.append(info.packageName).append("\n");
-//        }
-
     }
+
+    private static final int REQUEST_PROVISION_MANAGED_PROFILE = 1;
+
+    private void provisionManagedProfile() {
+        Intent intent = new Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE);
+        intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_PACKAGE_NAME,
+                mActivity.getPackageName());
+        if (intent.resolveActivity(mActivity.getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_PROVISION_MANAGED_PROFILE);
+            mActivity.finish();
+        } else {
+            Toast.makeText(mActivity, "Device provisioning is not enabled. Stopping.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
+
