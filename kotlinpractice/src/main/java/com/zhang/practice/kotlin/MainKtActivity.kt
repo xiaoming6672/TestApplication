188898d3c1d0a1ca72480d0b7bf3e1549d27954d
xiaoming6672 <zhangxiaoming6672@163.com>
2023-05-08 17:23:26 +0800

    创建kotlin学习module

---
+++ kotlinpractice/src/main/java/com/zhang/practice/kotlin/MainKtActivity.kt
@@ -0,0 +1,11 @@
+package com.zhang.practice.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainKtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kt)
+    }
+}
