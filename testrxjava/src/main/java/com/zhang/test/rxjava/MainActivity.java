c42fc028b1e14bfc2d27725b11be9d4a404a8db9
xiaoming6672 <zhangxiaoming6672@163.com>
2023-05-11 20:23:35 +0800

    RxJava学习 2023年5月11日

--- testrxjava/src/main/java/com/zhang/test/rxjava/MainActivity.java
+++ testrxjava/src/main/java/com/zhang/test/rxjava/MainActivity.java
@@ -1,9 +1,9 @@
 package com.zhang.test.rxjava;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
@@ -11,8 +11,12 @@ import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.functions.Predicate;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +32,58 @@ public class MainActivity extends AppCompatActivity {
                 emitter.onComplete();
             }
         };
-        Observable.create(source)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
+//        Observable.create(source)
+        Observable<String> observable = Observable.fromArray("01", "02", "03A")
+//                .flatMap(new Function<String, ObservableSource<String>>() {
+//                    @Override
+//                    public ObservableSource<String> apply(String s) throws Throwable {
+//                        return null;
+//                    }
+//                })
+                .filter(new Predicate<String>() {
+                    /**
+                     * Test the given input value and return a
+                     * boolean.
+                     *
+                     * @param s the value
+                     *
+                     * @return the boolean result
+                     * @throws Throwable if the implementation
+                     *                   wishes to throw any type of
+                     *                   exception
+                     */
                     @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+                    public boolean test(String s) throws Throwable {
+                        return !s.contains("A");
                     }
+                })
+                .map(new Function<String, String>() {
+                    @Override
+                    public String apply(String s) throws Throwable {
+                        return s + "_123";
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+        observable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe()");
+            }
 
-                    @Override
-                    public void onNext(@NonNull String s) {
-                    }
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.i(TAG, "onNext: s = " + s);
+            }
 
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                    }
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG, "onError: e = " + e.toString());
+            }
 
-                    @Override
-                    public void onComplete() {
-                    }
-                });
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete()");
+            }
+        });
     }
 }
