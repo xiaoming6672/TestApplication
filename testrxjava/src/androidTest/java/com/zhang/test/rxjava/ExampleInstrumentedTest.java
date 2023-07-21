cc716080e99969ee0de8619c9ea6d8ca91448c2b
xiaoming6672 <zhangxiaoming6672@163.com>
2023-07-21 09:47:23 +0800

    RxJava联系调试，fromIterable + filter + map + buffer

--- testrxjava/src/androidTest/java/com/zhang/test/rxjava/ExampleInstrumentedTest.java
+++ testrxjava/src/androidTest/java/com/zhang/test/rxjava/ExampleInstrumentedTest.java
@@ -3,19 +3,22 @@ package com.zhang.test.rxjava;
 import android.content.Context;
 import android.util.Log;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 import static org.junit.Assert.assertEquals;
@@ -104,6 +107,45 @@ public class ExampleInstrumentedTest {
                         Log.d("ZHANG", "accept>>>s=" + s);
                     }
                 });
+    }
+
+    @Test
+    public void rxFromIterable() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        Disposable disposable = Observable.fromIterable(list)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Throwable {
+                        return integer % 3 != 0;
+                    }
+                })
+                .concatMap(new Function<Integer, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(Integer integer) throws Throwable {
+                        String str = "str:" + integer;
+                        return Observable.just(str);
+                    }
+                })
+                .buffer(3)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<String>>() {
+                    @Override
+                    public void accept(List<String> list) throws Throwable {
+                        Log.d("ZHANG", "list.size = " + list.size() + "  list = " + list);
+                    }
+                });
+
+//                .subscribe(new Consumer<Object>() {
+//                    @Override
+//                    public void accept(Object object) throws Throwable {
+//                        Log.d("ZHANG", "accept>>>object=" + object);
+//                    }
+//                })
     }
 
 }
